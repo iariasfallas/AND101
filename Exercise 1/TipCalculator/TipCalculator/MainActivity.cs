f456216ed05c22c5e9050b1330eb28c3d5d1a545
Israel Arias Fallas <israel.arias@grupobabel.com>
2017-10-14 17:56:50 -0600

    AND101 - Exercise #3 - Code Behavior

--- Exercise 1/TipCalculator/TipCalculator/MainActivity.cs
+++ Exercise 1/TipCalculator/TipCalculator/MainActivity.cs
@@ -1,19 +1,45 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace TipCalculator
 {
     [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        Button calculateButton;
+        EditText inputBill;
+        TextView outputTip;
+        TextView outputTotal;
+
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
+
+        }
+
+        private void OnCalculateClick(object sender, EventArgs e)
+        {
+            double bill, tip, total;
+         
+            bill = double.Parse(inputBill.Text);
+            tip = bill * 0.15;
+            total = bill + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
         }
     }
 }
